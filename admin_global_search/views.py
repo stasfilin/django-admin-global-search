828d97b5f63767bf1582063d1321e70a32094247
Stanislav Filin <stasfilin@hotmail.com>
2024-02-10 11:11:11 +0000

    feat: change views.py
    Signed-off-by: Stanislav Filin <stasfilin@hotmail.com>

--- admin_global_search/views.py
+++ admin_global_search/views.py
@@ -7,43 +7,77 @@ from django.urls import reverse
 
 
 class GlobalSearchView(LoginRequiredMixin, View):
+    def get_query(self):
+        return self.request.GET.get("q", "")
+
+    def get_content_types_with_search_fields(self):
+        return ContentType.objects.filter(
+            id__in=[
+                ct.id
+                for ct in ContentType.objects.all()
+                if hasattr(ct.model_class(), "global_search_fields")
+                and ct.model_class() is not None
+            ]
+        )
+
+    def user_has_permission(self, model):
+        perm = f"{model.app_label}.view_{model.model}"
+        return self.request.user.has_perm(perm)
+
+    def build_search_query(self, search_fields, query):
+        q_objects = Q()
+        for field in search_fields:
+            q_objects |= Q(**{f"{field}__icontains": query})
+        return q_objects
+
+    def fetch_model_results(self, model_class, q_objects):
+        return model_class.objects.filter(q_objects)
+
+    def format_results(self, model, model_results):
+        model_key = f"{model.app_label}:{model.model}"
+        formatted_results = []
+        for result in model_results:
+            admin_url = reverse(
+                f"admin:{model.app_label}_{model.model}_change",
+                args=(result.pk,),
+            )
+            formatted_results.append(
+                {
+                    "model": model.model,
+                    "app_label": model.app_label,
+                    "object_id": result.pk,
+                    "str": str(result),
+                    "admin_url": admin_url,
+                }
+            )
+        return model_key, formatted_results
+
+    def process_model_for_search_results(self, model, query):
+        if not self.user_has_permission(model):
+            return None
+        model_class = model.model_class()
+        q_objects = self.build_search_query(model_class.global_search_fields, query)
+        model_results = self.fetch_model_results(model_class, q_objects)
+        if model_results.exists():
+            return self.format_results(model, model_results)
+        return None
+
     def get(self, request, *args, **kwargs):
-        query = request.GET.get("q", "")
+        query = self.get_query()
+        if not query:
+            return render(
+                request,
+                "admin_global_search/results.html",
+                {"query": "", "results": {}},
+            )
+
         results = {}
-        if query:
-            for model in ContentType.objects.all():
-                model_class = model.model_class()
-                if model_class is None or not hasattr(model_class, "global_search_fields"):
-                    continue
-                perm = f"{model.app_label}.view_{model.model}"
-                if not request.user.has_perm(perm):
-                    continue
-
-                search_fields = model_class.global_search_fields
-                q_objects = Q()
-                for field in search_fields:
-                    q_objects |= Q(**{f"{field}__icontains": query})
-
-                model_results = model_class.objects.filter(q_objects)
-                if model_results:
-                    model_key = f"{model.app_label}:{model.model}"
-                    if model_key not in results:
-                        results[model_key] = []
-
-                    for result in model_results:
-                        admin_url = reverse(
-                            f"admin:{model.app_label}_{model.model}_change",
-                            args=(result.pk,),
-                        )
-                        results[model_key].append(
-                            {
-                                "model": model.model,
-                                "app_label": model.app_label,
-                                "object_id": result.pk,
-                                "str": str(result),
-                                "admin_url": admin_url,
-                            }
-                        )
+        content_types = self.get_content_types_with_search_fields()
+        for model in content_types:
+            search_result = self.process_model_for_search_results(model, query)
+            if search_result:
+                model_key, formatted_results = search_result
+                results.setdefault(model_key, []).extend(formatted_results)
 
         return render(
             request,
